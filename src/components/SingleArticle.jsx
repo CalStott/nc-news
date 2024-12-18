6868ffad4d213ada84507184a6d9eab7a0133292
CalStott <caleb.stott@outlook.com>
2024-12-18 11:47:50 +0000

    view a list of comments associated with an article

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,10 +1,10 @@
 import { useParams } from 'react-router';
 import { useState, useEffect } from 'react';
 import { getArticleById, getCommentsByArticleId } from '../api';
+import { CommentsCard } from './CommentsCard';
 
 export const SingleArticle = () => {
-	const params = useParams();
-	const { article_id } = params;
+	const { article_id } = useParams();
 	const [article, setArticle] = useState({});
 	const [allComments, setAllComments] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
@@ -29,7 +29,7 @@ export const SingleArticle = () => {
 	return isError ? (
 		<p className="error-display">There has been an error!</p>
 	) : isLoading ? (
-		<p className="loading-display">Loading Articles!</p>
+		<p className="loading-display">Loading Article!</p>
 	) : (
 		<>
 			<section className="single-article">
@@ -37,14 +37,17 @@ export const SingleArticle = () => {
 					<h2>{article.title}</h2>
 					<p className="single-article-author">{article.author}</p>
 				</div>
-				<div>
+				<div className="article-main">
 					<img src={article.article_img_url} alt={article.title} />
-				</div>
-				<div>
 					<p className="single-article-body">{article.body}</p>
 				</div>
 			</section>
-			<section></section>
+			<section className="article-comments">
+				<h2>Comments</h2>
+				{allComments.map((comment) => {
+					return <CommentsCard comment={comment} key={comment.comment_id} />;
+				})}
+			</section>
 		</>
 	);
 };
